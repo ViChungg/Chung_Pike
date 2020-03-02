c80b33028a4edfd670d13bb9ae20e15c85fc9f35
Vicky <vickychungg@gmail.com>
2020-03-02 13:42:24 -0800

    Added Reading class and functionality to add a Reading to Firebase.

--- app/src/main/java/com/example/chung_pike/MainActivity.java
+++ app/src/main/java/com/example/chung_pike/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.chung_pike;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button addReading = findViewById(R.id.add_reading_btn);
+        addReading.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, AddReadingActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
